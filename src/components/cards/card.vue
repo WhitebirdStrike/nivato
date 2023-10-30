bb26fc82601cd6b07550c75cf4db20e3328247b4
WhitebirdStrike <144002849+WhitebirdStrike@users.noreply.github.com>
2023-10-30 16:02:20 +0100

    Holdt på med kort og v model
    Skal fortsette med v-for neste gang

---
+++ src/components/cards/card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="trollcard">
+    <div class="container">
+      <input type="“text”" v-model="tittel" />
+      <h2>
+        <b>Troll</b>
+      </h2>
+      <img src="@/images/Trololol.png" alt="Trolled" style="width: 25%" />
+      <p>The big troll</p>
+      <div v-for="(item, index) in myEpicArray" :key="index">
+        <button>{{ item.title }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const tittel = ref("Trollface");
+
+const myEpicArray = ref([
+  { title: "knapp 1" },
+  { title: "knapp 2" },
+  { title: "knapp 3" },
+]);
+</script>
+<style scoped>
+.trollcard {
+  border: 1px solid black;
+  margin: 2px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>{{ count }}</p>
+  <button @click="count++"></button>
+  <main>
+    <section class="flexitup">
+      <TrollCard />
+      <TrollCard />
+      <TrollCard />
+    </section>
+    <section class="flexitup">
+      <TrollCard />
+      <TrollCard />
+    </section>
+  </main>
+</template>
+
+<script setup>
+/*
+imports
+*/
+
+import { ref } from "vue";
+import TrollCard from "@/components/cards/card.vue";
+
+/*
+counter
+*/
+
+const count = ref(0);
+</script>
+
+<style scoped>
+.flexitup {
+  display: flex;
+}
+</style>
